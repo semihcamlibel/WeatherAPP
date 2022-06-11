0e8b005a24edac33044739fd9d3b2e499f537095
semihcamlibel <105104530+semihcamlibel@users.noreply.github.com>
2022-06-11 03:18:06 +0300

    Last

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // const opencageKey = 'c780dc47c66c42b2bffc687e9fb62a6d';
-const key = "5ea545d6d1b049d3a1d115918222805";
-const geoCodingBaseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
-const mapsBaseUrl = 'https://maps.googleapis.com/maps/api/place/photo?latlng='
+const weatherApiKey = "5ea545d6d1b049d3a1d115918222805";
+const geoCodingBaseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
+const mapsBaseUrl = 'https://maps.googleapis.com/maps/api/place/photo?latlng=';
+const placeBaseUrl = 'https://maps.googleapis.com/maps/api/place/';
 const weatherApiBaseUrl = 'https://api.weatherapi.com/v1';
 const googleKey = 'AIzaSyCWThpogVzskhqv5em8s5lS1D9w2rNNquE';
 
@@ -21,9 +22,11 @@ function success(position) {
   }
 }
 
+
 const getLocate = async (latitude, longitude) => {
-  var GEOCODING = geoCodingBaseUrl + latitude + '%2C' + longitude + '&language=en,+CA&key=' + googleKey + '';
-  var MAPS = mapsBaseUrl + latitude + '%2C' + longitude + '&language=en,+CA&key=' + googleKey + '';
+
+  var GEOCODING = `${geoCodingBaseUrl}${latitude}%2C${longitude}&language=en,+CA&key=${googleKey}`;
+  var MAPS = `${mapsBaseUrl}${latitude}%2C${longitude}&language=en,+CA&key=${googleKey}`;
   const response = await fetch(GEOCODING)
     .then((data) => data.json())
     .catch((error) => console.log(error));
@@ -39,15 +42,15 @@ function error(err) {
 
 const getWeather = async (city) => {
   const weatherEndPoint = '/current.json';
-  const requestParams = `?key=${key}&q=${city}&aqi=yes`;
-  const urlToFetch = weatherApiBaseUrl + weatherEndPoint + requestParams;
+  const requestParams = `?key=${weatherApiKey}&q=${city}&aqi=yes`;
+  const urlToFetch = `${weatherApiBaseUrl}${weatherEndPoint}${requestParams}`;
   const response = await fetch(urlToFetch)
     .then((data) => data.json())
     .catch((error) => console.log(error));
 
 
   const {
-    location: { localtime: localtime },
+    location: { localtime: localTime },
     current: { temp_c: degree },
     location: { name: cityName },
     location: { country: countryName },
@@ -63,7 +66,7 @@ const getWeather = async (city) => {
 
   } = response.current;
 
-  document.getElementById('localtime').innerHTML = 'Local Time in ' + cityName + '<br>' + ' ' + localtime;
+  document.getElementById('localtime').innerHTML = 'Local Time in ' + cityName + '<br>' + ' ' + localTime;
   document.getElementById('degree').innerHTML = degree + '°';
   document.getElementById('location').innerHTML = cityName;
   document.getElementById('conditions').innerHTML = condText;
@@ -72,6 +75,7 @@ const getWeather = async (city) => {
   document.getElementById('humidity').innerHTML = 'Humidity: ' + humidity + ' %';
   document.getElementById('windspeed').innerHTML = 'Wind speed: ' + windSpeed + ' km/h';
   document.getElementById('flagimg').src = 'https://countryflagsapi.com/png/' + countryName;
+
 
   //CARD DIV BACGROUND 
   if (condText === 'Sunny' && isDay === 1) {
@@ -90,28 +94,43 @@ const getWeather = async (city) => {
   return response;
 };
 
-// GOOGLE MAPS API - GET PHOTO REF WITH CITY NAME
+
+
+// GOOGLE MAPS API - GET PHOTO REF WITH CITY NAME - NOT WORKING BEACUSE CORS
 const getPhotoRef = async (photoCity) => {
-  const photoRefFetch = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' + photoCity + ',%20IL&key=' + googleKey + '&inputtype=textquery&fields=name,photos'
+  const photoRefFetch = `${placeBaseUrl}findplacefromtext/json?input=${photoCity},%20IL&key=${googleKey}&inputtype=textquery&fields=name,photos`;
   const response = await fetch(photoRefFetch, {
   })
   //const photoref = (photoResponse.candidates[0].photos[0].photo_reference);
   getPhoto(photoref);
 }
 
-
 //GOOGLE MAPS API - GET PHOTO WITH PHOTO REF - NOT WORKING BEACUSE CORS
 const getPhoto = async (photoref) => {
-  const photoFetch = 'https://maps.googleapis.com/maps/api/place/photo?photoreference=' + photoref + '&key=' + googleKey + '&maxwidth=1920&maxheight=1080';
+  const photoFetch = `${placeBaseUrl}photo?photoreference=${photoref}&key=${googleKey}&maxwidth=1920&maxheight=1080`;
   document.body.style.backgroundImage = "url(" + photoFetch + ")";
 }
 
+
+
 // INDEX SELECT
 const getSelectedWeather = () => {
-  let select = document.getElementById('selectedcity');
-  let selectedValue = select.options[select.selectedIndex].value;
-  getWeather("'" + selectedValue + "'");
+  const select = document.getElementById('selectedcity');
+  const selectedValue = select.options[select.selectedIndex].value;
+  getWeather(`'${selectedValue}'`);
 }
+
+// TURKEY CITIES ARRAY
+const cities = ["Adana", "Adıyaman", "Afyon", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin", "Aydin", "Balıkesir", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Isparta", "İçel (Mersin)", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Mugla", "Muş", "Nevşehir", "Niğde", "Ordu", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Sirnak", "Bartin", "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Duzce"
+];
+
+for (let i = 0; i < cities.length; i++) {
+  document.getElementById('selectedcity').innerHTML += `<option value="${cities[i]}"> ${cities[i]} </option>`;
+}
+
+
+
+
 
 // OTHER LOCATE API
 /*
@@ -134,12 +153,3 @@ const tryOpenCage = async (latitude, longitude) => {
   }
 }
 */
-
-// TURKEY CITIES ARRAY
-const cities = ["Adana", "Adıyaman", "Afyon", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin", "Aydin", "Balıkesir", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Isparta", "İçel (Mersin)", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Mugla", "Muş", "Nevşehir", "Niğde", "Ordu", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Sirnak", "Bartin", "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Duzce"
-];
-
-for (let i = 0; i < cities.length; i++) {
-  document.getElementById('selectedcity').innerHTML += '<option value="' + cities[i] + '">' + cities[i] + '</option>';
-}
-
